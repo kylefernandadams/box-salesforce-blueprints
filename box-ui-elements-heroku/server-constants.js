1c1b76c5bc42562e610ca9c8b2517633e42234d8
Kyle Adams <kyle.adams@massnerder.io>
2022-03-28 19:51:41 -0400

    Separate react project

--- box-ui-elements-heroku/server-constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    EXPLORER_SCOPES: "base_explorer item_preview item_download item_rename item_delete item_share item_upload root_readwrite annotation_view_all annotation_edit",
-    RECENTS_SCOPES: "base_explorer item_preview item_download item_rename item_delete item_share item_upload root_readwrite annotation_view_all annotation_edit" ,
-    PICKER_SCOPES: "base_picker item_preview item_share item_upload",
-    UPLOADER_SCOPES: "base_upload",
-    PREVIEW_SCOPES: "base_preview item_download item_upload annotation_view_all annotation_edit root_readwrite"
-}
--- box-ui-elements-heroku/src/Constants.js
+++
@@ -1,3 +0,0 @@
-export const THEME_COLOR = "#000000";
-export const EXPRESS_SERVER_HOST = `https://${process.env.REACT_APP_HOST}.herokuapp.com`;
-// export const EXPRESS_SERVER_HOST = "http://localhost:8080";
